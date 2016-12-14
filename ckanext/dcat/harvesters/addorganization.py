220ab6f40018a8f8fe6ad4014a1bb215c5166e8c
Shanshan Jiang <shanshan.jiang@sintef.no>
2016-12-14 09:28:43 +0100

    clean up

--- ckanext/dcat/harvesters/addorganization.py
+++ ckanext/dcat/harvesters/addorganization.py
@@ -1,23 +1,22 @@
 # Description: get harvested organizations from data.norge.no and save to orgfile.txt
-# Author: Shanshan Jiang, last modified 13.12.2016
+# Author: Shanshan Jiang, last modified 14.12.2016
 
 import json
 import urllib
 print "test"
-url='http://78.91.98.234:5000/api/3/action/organization_list'
+url='http://127.0.0.1:5000/api/3/action/organization_list'
 content=urllib.urlopen(url).read()
 
 orgfile = open('orgfile.txt', 'w')
 
 def _get_organizations(content):
-    # Todo: Test that this works before using it in other methods!!
     doc = json.loads(content)
     datasets = doc["result"]
     print datasets
     for dataset in datasets:
         print dataset
         orgfile.write('org_name:' + str(dataset) + '\n')
-        showorgurl='http://78.91.98.234:5000/api/3/action/organization_show?id=' + dataset
+        showorgurl='http://127.0.0.1:5000/api/3/action/organization_show?id=' + dataset
         _get_org_details(showorgurl)
         print '**************'
 
@@ -28,7 +27,6 @@ def _get_org_details(url):
     image_url = datasets.get('image_url')
     print image_url
     orgfile.write('url:' + str(image_url) + '\n')
-    #orgfile.write('display_name:' + datasets.get('display_name').get('value') + '\n')
     orgfile.write('display_name:' + datasets.get('display_name').encode('utf8') + '\n')
     
 
